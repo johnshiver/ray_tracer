4d955c0fde6d1a9f4e27957e4bda245cb056476c
John Shiver <johneshiver@gmail.com>
2019-11-17 23:50:54 -0500

    added intersections collections

--- src/rays.rs
+++ src/rays.rs
@@ -1,8 +1,12 @@
 use uuid::Uuid;
 
 use crate::tuple::{Tuple, TupleTypeError, new_point, dot};
+use std::ops::Index;
+use std::borrow::Borrow;
+
 pub const SPHERE_ORIGIN: Tuple = Tuple { x: 0.0, y: 0.0, z: 0.0, w: 1.0 }; // is a point
 
+#[derive(Debug)]
 pub struct Ray {
     origin: Tuple,    // point
     direction: Tuple, // vector
@@ -26,8 +30,15 @@ pub fn position(r: &Ray, time: f64) -> Tuple {
     r.origin + r.direction * time
 }
 
+#[derive(Debug,Clone)]
 pub struct Sphere {
     pub id: Uuid,
+}
+
+impl PartialEq for Sphere {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
 }
 
 pub fn new_sphere() -> Sphere {
@@ -37,9 +48,34 @@ pub fn new_sphere() -> Sphere {
 
 }
 
-pub struct Intersection {
+#[derive(Debug)]
+pub struct Intersection<T> {
+    pub t: f64,           // value of intersection
+    pub object: T,    // object that was intersected
+}
+
+pub fn new_intersection(t:f64, object: Sphere) -> Intersection<Sphere> {
+    Intersection{
+        t, object
+    }
+}
+
+pub struct Intersections<T> {
+    items: Vec<Intersection<T>>,
     count: i64,
-    pub positions: [f64; 2],
+}
+
+impl Index<usize> for Intersections<Sphere> {
+    type Output = Intersection<Sphere>;
+    fn index(&self, index: usize) -> &Self::Output {
+        self.items[index].borrow()
+    }
+}
+
+pub fn intersections(items: Vec<Intersection<Sphere>>, count: i64) -> Intersections<Sphere> {
+    Intersections{
+        items, count
+    }
 }
 
 pub fn discriminant(r: &Ray) -> f64  {
@@ -51,10 +87,10 @@ pub fn discriminant(r: &Ray) -> f64  {
 }
 
 // returns set of t values, where ray intersects sphere
-pub fn intersect(r: &Ray, s: Sphere) -> ([f64; 2], i16) {
+pub fn intersect(r: &Ray, s: Sphere) -> Intersections<Sphere> {
     let d = discriminant(r);
     if d < 0.0 {
-        return ([0.0; 2], 0)
+        return intersections(vec![], 0)
     }
     let sphere_to_ray = r.origin - SPHERE_ORIGIN;
     let a = dot(r.direction, r.direction).unwrap();
@@ -63,18 +99,22 @@ pub fn intersect(r: &Ray, s: Sphere) -> ([f64; 2], i16) {
     let t1 = (-b - d.sqrt()) / (2.0 * a);
     let t2 = (-b + d.sqrt()) / (2.0 * a);
 
+    // TODO: copying here because I dont understand lifetime stuff :/
+    let i1 = new_intersection(t1, s.clone());
+    let i2 = new_intersection(t2, s.clone());
+
     let mut count = 0;
     if t1 == t2 {
-        return ([t1, t2], 1)
+        return intersections(vec![i1, i2], 1)
     }
-    ([t1, t2], 2)
+    intersections(vec![i1, i2], 2)
 }
 
 
 #[cfg(test)]
 mod tests {
 use crate::tuple::{new_point, new_vector};
-    use crate::rays::{new_ray, position, new_sphere, intersect};
+    use crate::rays::{new_ray, position, new_sphere, intersect, new_intersection, intersections};
     use std::borrow::Borrow;
 
     #[test]
@@ -99,51 +139,71 @@ use crate::tuple::{new_point, new_vector};
     fn ray_intersects_sphere_two_pts() {
         let r = new_ray(new_point(0.0, 0.0, -5.0), new_vector(0.0, 0.0, 1.0)).unwrap();
         let s = new_sphere();
-        let (xs, count) = intersect(r.borrow(), s);
-        assert_eq!(count, 2);
-        assert_eq!(xs[0], 4.0);
-        assert_eq!(xs[1], 6.0);
+        let xs = intersect(r.borrow(), s);
+        assert_eq!(xs.count, 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
     }
 
     #[test]
     fn ray_intersects_sphere_at_tangent() {
         let r = new_ray(new_point(0.0, 1.0, -5.0), new_vector(0.0, 0.0, 1.0)).unwrap();
         let s = new_sphere();
-        let (xs, count) = intersect(r.borrow(), s);
-        assert_eq!(count, 1);
+        let xs  = intersect(r.borrow(), s);
+        assert_eq!(xs.count, 1);
         // assuming two intersections for simplicity
-        assert_eq!(xs[0], 5.0);
-        assert_eq!(xs[1], 5.0);
+        assert_eq!(xs[0].t, 5.0);
+        assert_eq!(xs[1].t, 5.0);
     }
 
     #[test]
     fn ray_misses_sphere() {
         let r = new_ray(new_point(0.0, 2.0, -5.0), new_vector(0.0, 0.0, 1.0)).unwrap();
         let s = new_sphere();
-        let (xs, count) = intersect(r.borrow(), s);
-        assert_eq!(count, 0);
+        let xs = intersect(r.borrow(), s);
+        assert_eq!(xs.count, 0);
     }
 
     #[test]
     fn ray_originates_inside_sphere() {
         let r = new_ray(new_point(0.0, 0.0, 0.0), new_vector(0.0, 0.0, 1.0)).unwrap();
         let s = new_sphere();
-        let (xs, count) = intersect(r.borrow(), s);
-        assert_eq!(count, 2);
+        let xs  = intersect(r.borrow(), s);
+        assert_eq!(xs.count, 2);
         // assuming two intersections for simplicity
-        assert_eq!(xs[0], -1.0);
-        assert_eq!(xs[1], 1.0);
+        assert_eq!(xs[0].t, -1.0);
+        assert_eq!(xs[1].t, 1.0);
     }
 
     #[test]
     fn sphere_is_behind_ray() {
         let r = new_ray(new_point(0.0, 0.0, 5.0), new_vector(0.0, 0.0, 1.0)).unwrap();
         let s = new_sphere();
-        let (xs, count) = intersect(r.borrow(), s);
-        assert_eq!(count, 2);
+        let xs  = intersect(r.borrow(), s);
+        assert_eq!(xs.count, 2);
         // assuming two intersections for simplicity
-        assert_eq!(xs[0], -6.0);
-        assert_eq!(xst[1], -4.0);
+        assert_eq!(xs[0].t, -6.0);
+        assert_eq!(xs[1].t, -4.0);
+    }
+
+    #[test]
+    fn intersection_encapsulates_t_object() {
+        let s = new_sphere();
+        let i = new_intersection(3.5, s.clone());
+        assert_eq!(i.t, 3.5);
+        assert_eq!(i.object, s);
+    }
+
+    #[test]
+    fn aggregating_intersections() {
+        let s = new_sphere();
+        let i1 = new_intersection(1.0, s.clone());
+        let i2 = new_intersection(2.0, s.clone());
+
+        let xs = intersections(vec![i1, i2], 2);
+        assert_eq!(xs[0].t, 1.0);
+        assert_eq!(xs[1].t, 2.0);
+        assert_eq!(xs.count, 2);
     }
 
 }
