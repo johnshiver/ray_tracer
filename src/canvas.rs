f004c23be6dffe2bc2281e483a509b8ab3102d70
John Shiver <john@johnshiver.org>
2022-08-13 15:39:49 -0400

    add everything back

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,168 +1,168 @@
-// use std::fs::File;
-// use std::io::Write;
-// use std::path::Path;
-//
-// use crate::color::{new_color, Color};
-//
-// pub struct Canvas {
-//     height: usize,
-//     width: usize,
-//     pixels: Vec<Vec<Color>>,
-// }
-//
-// impl Canvas {
-//     pub fn get_pixel(&self, x: usize, y: usize) -> Option<Color> {
-//         if x < self.width && y < self.height {
-//             Some(self.pixels[y][x])
-//         } else {
-//             None
-//         }
-//     }
-//
-//     pub fn write_pixel(&mut self, x: usize, y: usize, color: Color) -> bool {
-//         if x < self.width && y < self.height {
-//             // adjust y to invert graph
-//             // let adjusted_y = self.height - y - 1;
-//             self.pixels[y][x] = color;
-//             true
-//         } else {
-//             false
-//         }
-//     }
-//
-//     pub fn to_ppm(&self, filename: &str) -> std::io::Result<()> {
-//         let path = Path::new(filename);
-//         let mut file = File::create(&path)?;
-//         let _ = file.write(self.get_ppm_header().as_bytes())?;
-//         let _ = file.write(self.get_ppm_pixel_data().as_bytes())?;
-//         Ok(())
-//     }
-//
-//     pub fn get_ppm_header(&self) -> String {
-//         format!("P3\n{} {}\n255\n", self.width, self.height)
-//     }
-//
-//     pub fn get_ppm_pixel_data(&self) -> String {
-//         let mut pixel_data = String::new();
-//         let mut curr_line_len = 0;
-//         let max_pixel_length = 11;
-//         let max_ppm_line = 70;
-//         for y in 0..self.height {
-//             for x in 0..self.width {
-//                 let pixel = self.pixels[y][x];
-//                 let format_string = format!("{}", pixel);
-//                 pixel_data.push_str(format_string.as_ref());
-//                 curr_line_len += format_string.len();
-//                 if curr_line_len + max_pixel_length > max_ppm_line {
-//                     pixel_data.push('\n');
-//                     curr_line_len = 0;
-//                 } else if x != self.width - 1 {
-//                     pixel_data.push(' ')
-//                 }
-//             }
-//             if y != self.height - 1 {
-//                 pixel_data.push(' ');
-//             } else {
-//                 pixel_data.push('\n');
-//             }
-//         }
-//         pixel_data
-//     }
-// }
-//
-// pub fn new_canvas(width: usize, height: usize) -> Canvas {
-//     let default = new_color(0.0, 0.0, 0.0);
-//     let pixels = vec![vec![default; width]; height];
-//     Canvas {
-//         height,
-//         width,
-//         pixels,
-//     }
-// }
-//
-// #[cfg(test)]
-// mod tests {
-//     use crate::canvas::new_canvas;
-//     use crate::color::new_color;
-//
-//     #[test]
-//     fn create_canvas() {
-//         let width = 30;
-//         let height = 30;
-//         let test_canvas = new_canvas(width, height);
-//         let expected = new_color(0.0, 0.0, 0.0);
-//         for y in 0..height {
-//             for x in 0..width {
-//                 match test_canvas.get_pixel(x, y) {
-//                     None => panic!("get_pixel should have returned a color x: {} y: {}", x, y),
-//                     Some(pixel_color) => assert_eq!(expected, pixel_color),
-//                 }
-//             }
-//         }
-//     }
-//
-//     #[test]
-//     fn write_pixel() {
-//         let width = 30;
-//         let height = 30;
-//         let mut test_canvas = new_canvas(width, height);
-//         let expected = new_color(1.0, 0.0, 0.0);
-//         let written = test_canvas.write_pixel(2, 3, expected);
-//         assert!(written);
-//         match test_canvas.get_pixel(2, 3) {
-//             Some(color) => assert_eq!(expected, color),
-//             None => panic!("get_pixel should not have returned an error"),
-//         }
-//     }
-//
-//     #[test]
-//     fn ppm_header() {
-//         let width = 5;
-//         let height = 3;
-//         let test_canvas = new_canvas(width, height);
-//         let expected = "P3\n5 3\n255\n";
-//         assert_eq!(expected, test_canvas.get_ppm_header())
-//     }
-//
-//     // these features seem to work pretty well
-//     // even tho tests fail i will ignore for now
-//
-//     //    #[test]
-//     //    fn ppm_pixel_data() {
-//     //        let width = 5;
-//     //        let height = 3;
-//     //        let mut test_canvas = new(width, height);
-//     //        let c1 = new_color(1.5, 0.0, 0.0);
-//     //        let c2 = new_color(0.0, 0.5, 0.0);
-//     //        let c3 = new_color(-0.5, 0.0, 1.0);
-//     //        let written = test_canvas.write_pixel(0, 0, c1);
-//     //        assert_eq!(true, written);
-//     //        let written = test_canvas.write_pixel(2, 1, c2);
-//     //        assert_eq!(true, written);
-//     //        let written = test_canvas.write_pixel(4, 2, c3);
-//     //        assert_eq!(true, written);
-//     //
-//     //        let expected = "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 255\n";
-//     //        assert_eq!(expected, test_canvas.get_ppm_pixel_data())
-//     //    }
-//     //
-//     //    #[test]
-//     //    fn splitting_long_line_ppms() {
-//     //        let width = 10;
-//     //        let height = 2;
-//     //        let mut test_canvas = new(width, height);
-//     //        let c1 = new_color(1.0, 0.8, 0.6);
-//     //        for x in 0..width {
-//     //            for y in 0..height {
-//     //                let written = test_canvas.write_pixel(x, y, c1);
-//     //                assert_eq!(true, written);
-//     //            }
-//     //        }
-//     //
-//     //        let expected = "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n153 255 204 153 255 204 153 255 204 153 255 204 153\n255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n153 255 204 153 255 204 153 255 204 153";
-//     //        let pixel_data = test_canvas.get_ppm_pixel_data();
-//     //        let lines: Vec<&str> = pixel_data.split('\n').collect();
-//     //        let result = lines.join("\n");
-//     //        assert_eq!(expected, result)
-//     //    }
-// }
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+use crate::color::{Color, new_color};
+
+pub struct Canvas {
+    height: usize,
+    width: usize,
+    pixels: Vec<Vec<Color>>,
+}
+
+impl Canvas {
+    pub fn get_pixel(&self, x: usize, y: usize) -> Option<Color> {
+        if x < self.width && y < self.height {
+            Some(self.pixels[y][x])
+        } else {
+            None
+        }
+    }
+
+    pub fn write_pixel(&mut self, x: usize, y: usize, color: Color) -> bool {
+        if x < self.width && y < self.height {
+            // adjust y to invert graph
+            // let adjusted_y = self.height - y - 1;
+            self.pixels[y][x] = color;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn to_ppm(&self, filename: &str) -> std::io::Result<()> {
+        let path = Path::new(filename);
+        let mut file = File::create(&path)?;
+        let _ = file.write(self.get_ppm_header().as_bytes())?;
+        let _ = file.write(self.get_ppm_pixel_data().as_bytes())?;
+        Ok(())
+    }
+
+    pub fn get_ppm_header(&self) -> String {
+        format!("P3\n{} {}\n255\n", self.width, self.height)
+    }
+
+    pub fn get_ppm_pixel_data(&self) -> String {
+        let mut pixel_data = String::new();
+        let mut curr_line_len = 0;
+        let max_pixel_length = 11;
+        let max_ppm_line = 70;
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let pixel = self.pixels[y][x];
+                let format_string = format!("{}", pixel);
+                pixel_data.push_str(format_string.as_ref());
+                curr_line_len += format_string.len();
+                if curr_line_len + max_pixel_length > max_ppm_line {
+                    pixel_data.push('\n');
+                    curr_line_len = 0;
+                } else if x != self.width - 1 {
+                    pixel_data.push(' ')
+                }
+            }
+            if y != self.height - 1 {
+                pixel_data.push(' ');
+            } else {
+                pixel_data.push('\n');
+            }
+        }
+        pixel_data
+    }
+}
+
+pub fn new_canvas(width: usize, height: usize) -> Canvas {
+    let default = new_color(0.0, 0.0, 0.0);
+    let pixels = vec![vec![default; width]; height];
+    Canvas {
+        height,
+        width,
+        pixels,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::canvas::new_canvas;
+    use crate::color::new_color;
+
+    #[test]
+    fn create_canvas() {
+        let width = 30;
+        let height = 30;
+        let test_canvas = new_canvas(width, height);
+        let expected = new_color(0.0, 0.0, 0.0);
+        for y in 0..height {
+            for x in 0..width {
+                match test_canvas.get_pixel(x, y) {
+                    None => panic!("get_pixel should have returned a color x: {} y: {}", x, y),
+                    Some(pixel_color) => assert_eq!(expected, pixel_color),
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn write_pixel() {
+        let width = 30;
+        let height = 30;
+        let mut test_canvas = new_canvas(width, height);
+        let expected = new_color(1.0, 0.0, 0.0);
+        let written = test_canvas.write_pixel(2, 3, expected);
+        assert!(written);
+        match test_canvas.get_pixel(2, 3) {
+            Some(color) => assert_eq!(expected, color),
+            None => panic!("get_pixel should not have returned an error"),
+        }
+    }
+
+    #[test]
+    fn ppm_header() {
+        let width = 5;
+        let height = 3;
+        let test_canvas = new_canvas(width, height);
+        let expected = "P3\n5 3\n255\n";
+        assert_eq!(expected, test_canvas.get_ppm_header())
+    }
+
+    // these features seem to work pretty well
+    // even tho tests fail i will ignore for now
+
+    //    #[test]
+    //    fn ppm_pixel_data() {
+    //        let width = 5;
+    //        let height = 3;
+    //        let mut test_canvas = new(width, height);
+    //        let c1 = new_color(1.5, 0.0, 0.0);
+    //        let c2 = new_color(0.0, 0.5, 0.0);
+    //        let c3 = new_color(-0.5, 0.0, 1.0);
+    //        let written = test_canvas.write_pixel(0, 0, c1);
+    //        assert_eq!(true, written);
+    //        let written = test_canvas.write_pixel(2, 1, c2);
+    //        assert_eq!(true, written);
+    //        let written = test_canvas.write_pixel(4, 2, c3);
+    //        assert_eq!(true, written);
+    //
+    //        let expected = "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 255\n";
+    //        assert_eq!(expected, test_canvas.get_ppm_pixel_data())
+    //    }
+    //
+    //    #[test]
+    //    fn splitting_long_line_ppms() {
+    //        let width = 10;
+    //        let height = 2;
+    //        let mut test_canvas = new(width, height);
+    //        let c1 = new_color(1.0, 0.8, 0.6);
+    //        for x in 0..width {
+    //            for y in 0..height {
+    //                let written = test_canvas.write_pixel(x, y, c1);
+    //                assert_eq!(true, written);
+    //            }
+    //        }
+    //
+    //        let expected = "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n153 255 204 153 255 204 153 255 204 153 255 204 153\n255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n153 255 204 153 255 204 153 255 204 153";
+    //        let pixel_data = test_canvas.get_ppm_pixel_data();
+    //        let lines: Vec<&str> = pixel_data.split('\n').collect();
+    //        let result = lines.join("\n");
+    //        assert_eq!(expected, result)
+    //    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::f64::consts::PI;
 
-// use crate::canvas::new_canvas;
-// use crate::color::new_color;
-// use crate::environment::new_environment;
+use crate::canvas::new_canvas;
+use crate::color::new_color;
+use crate::environment::new_environment;
 use crate::matrix_transformations::rotation_y;
 use crate::projectile::new_projectile;
 use crate::tuple::{new_point, new_vector, normalize};
@@ -18,9 +18,9 @@ mod tuple;
 mod utils;
 
 fn main() {
-    // analog_clock();
+    analog_clock();
     // create_test_image();
-    // simulate_projectile();
+    simulate_projectile();
 }
 
 // fn create_test_image() {
@@ -40,56 +40,56 @@ fn main() {
 //     canvas.to_ppm("test_ppm.ppm").expect("while creating ppm");
 // }
 //
-// fn simulate_projectile() {
-//     let start = new_point(0.0, 1.0, 0.0);
-//     let velocity = new_vector(1.0, 1.8, 0.0) * 11.25;
-//     let velocity = normalize(&velocity).expect("while normalizing velocity");
-//     let mut p = new_projectile(start, velocity);
-//     let gravity = new_vector(0.0, -0.1, 0.0);
-//     let wind = new_vector(0.01, 0.0, 0.0);
-//     let mut c = new_canvas(900, 550);
-//     let env = new_environment(gravity, wind);
-//     let white = new_color(1.0, 1.0, 1.0);
-//
-//     let alpha = 40.0;
-//     c.write_pixel(
-//         (p.position.x * alpha) as usize,
-//         (p.position.y * alpha) as usize,
-//         white,
-//     );
-//     while p.position.x >= 0.0 && p.position.y >= 0.0 {
-//         p = environment::tick(env, p);
-//         println!(
-//             "projectile now at:\n\t{}\n\tvelocity {}",
-//             p.position, p.velocity
-//         );
-//         c.write_pixel(
-//             (p.position.x * alpha) as usize,
-//             (p.position.y * alpha) as usize,
-//             white,
-//         );
-//     }
-//     c.to_ppm("rocket_shot.ppm").expect("while creating ppm");
-// }
-//
-// fn analog_clock() {
-//     let width = 100;
-//     let height = 100;
-//     let rad = width as f64 * 0.45;
-//     let mut c = new_canvas(width, height);
-//     let white = new_color(1.0, 1.0, 1.0);
-//
-//     let origin = new_point(width as f64 / 2.0_f64, 0.0, height as f64 / 2.0_f64);
-//     let noon = new_point(0.0, 0.0, 1.0);
-//
-//     const HOUR: f64 = PI / 6.0_f64;
-//
-//     for i in 0..12 {
-//         let r = rotation_y(i as f64 * HOUR);
-//         let clock_hand = r * noon * rad;
-//         let final_pos = new_point(origin.x + clock_hand.x, 0.0, origin.z + clock_hand.z);
-//         c.write_pixel(final_pos.x as usize, final_pos.z as usize, white);
-//     }
-//
-//     c.to_ppm("analog_clock.ppm").expect("while creating ppm");
-// }
+fn simulate_projectile() {
+    let start = new_point(0.0, 1.0, 0.0);
+    let velocity = new_vector(1.0, 1.8, 0.0) * 11.25;
+    let velocity = normalize(&velocity);
+    let mut p = new_projectile(start, velocity);
+    let gravity = new_vector(0.0, -0.1, 0.0);
+    let wind = new_vector(0.01, 0.0, 0.0);
+    let mut c = new_canvas(900, 550);
+    let env = new_environment(gravity, wind);
+    let white = new_color(1.0, 1.0, 1.0);
+
+    let alpha = 40.0;
+    c.write_pixel(
+        (p.position.x * alpha) as usize,
+        (p.position.y * alpha) as usize,
+        white,
+    );
+    while p.position.x >= 0.0 && p.position.y >= 0.0 {
+        p = environment::tick(env, p);
+        println!(
+            "projectile now at:\n\t{}\n\tvelocity {}",
+            p.position, p.velocity
+        );
+        c.write_pixel(
+            (p.position.x * alpha) as usize,
+            (p.position.y * alpha) as usize,
+            white,
+        );
+    }
+    c.to_ppm("rocket_shot.ppm").expect("while creating ppm");
+}
+
+fn analog_clock() {
+    let width = 100;
+    let height = 100;
+    let rad = width as f64 * 0.45;
+    let mut c = new_canvas(width, height);
+    let white = new_color(1.0, 1.0, 1.0);
+
+    let origin = new_point(width as f64 / 2.0_f64, 0.0, height as f64 / 2.0_f64);
+    let noon = new_point(0.0, 0.0, 1.0);
+
+    const HOUR: f64 = PI / 6.0_f64;
+
+    for i in 0..12 {
+        let r = rotation_y(i as f64 * HOUR);
+        let clock_hand = r * noon * rad;
+        let final_pos = new_point(origin.x + clock_hand.x, 0.0, origin.z + clock_hand.z);
+        c.write_pixel(final_pos.x as usize, final_pos.z as usize, white);
+    }
+
+    c.to_ppm("analog_clock.ppm").expect("while creating ppm");
+}
